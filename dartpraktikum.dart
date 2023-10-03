632bbcb00fe3f61f7b6527709d6a069044688114
DescaMr <61442383+DescaMr@users.noreply.github.com>
2023-10-03 12:09:49 +0700

    penambahan tugas 1 praktikum

---
+++ dartpraktikum.dart
@@ -0,0 +1,5 @@
+import 'package:dartpraktikum/dartpraktikum.dart' as dartpraktikum;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartpraktikum.calculate()}!');
+}
---
+++ string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Membuat 3 variabel string
+  String string1 = "Desca";
+  String string2 = " Mochamad";
+  String string3 = " Rizal";
+
+  // Menggabungkan ketiga string
+  String hasilGabungan = string1 + string2 + string3;
+
+  // Menampilkan hasilnya di layar
+  print(hasilGabungan);
+}
